5b71b2458dac48f6e99fbe9314b4b34e9b1786db
Weiliyoung <459819025@qq.com>
2024-05-23 19:32:31 +0800

    first commit

---
+++ src/main/java/com/example/uninews_admin/UninewsAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.uninews_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UninewsAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UninewsAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uninews_admin/UninewsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uninews_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UninewsAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
